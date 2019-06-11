f0cf6fa0b4e89aee750cc72670ae4cd72bc907b7
Darby Dixon III <ddixon@oberlin.edu>
2019-06-11 12:14:15 -0400

    Sandboxed an array comboizer idea.

--- js/modules/helloThere.js
+++ js/modules/helloThere.js
@@ -1,6 +1,24 @@
 // Say hello! A tiny module!
 var helloWorld = function() {
   console.log("Hello from inside a module");
+
+  // sandbox stuff
+
+  let inputItems = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' ];
+  let outputItems = [];
+
+  for (let i = 0; i < inputItems.length - 1; i++) {
+    let x = inputItems[i];
+
+    for (let j = i + 1; j < inputItems.length; j++) {
+      let y = inputItems[j];
+      let output = [ x, y ];
+
+      outputItems.push(output);
+    }
+  }
+
+  console.log(outputItems);
 };
 
 module.exports = helloWorld;
